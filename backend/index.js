35cbde50f7371d7805a32537df581e5f5d01364b
singhsachinrajput6003 <singhsachinrajput6003@gmail.com>
2026-04-07 20:09:53 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const UserRouter = require('./routers/userRouter');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
